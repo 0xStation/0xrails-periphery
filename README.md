5e6bd30237f5dba37cf4faf62354987bebd9a708
Michael Gingras <mcg79@cornell.edu>
2023-04-26 17:09:17 -0600

    readme + deploy fixes

---
+++ README.md
@@ -0,0 +1,8 @@
+### installs
+
+- forge install
+
+### deploy Local
+
+- anvil
+- forge script script/Deploy.s.sol:MyScript --fork-url http://localhost:8545 --private-key $PRIVATE_KEY0 --broadcast
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -10,20 +10,18 @@ contract Deploy is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
-
-        address owner = 0x016562aA41A8697720ce0943F003141f5dEAe006;
-
-        address renderer = address(new Renderer(owner, "https://token.station.express/api/v1/nftMetadata"));
+        vm.startBroadcast();
+        address renderer = address(new Renderer(msg.sender, "https://token.station.express/api/v1/nftMetadata"));
         address badgeImpl = address(new Badge());
 
         bytes memory initData =
-            abi.encodeWithSelector(Badge(badgeImpl).init.selector, owner, renderer, "Friends of Station", "FRIENDS");
+            abi.encodeWithSelector(Badge(badgeImpl).init.selector, msg.sender, renderer, "Friends of Station", "FRIENDS");
         address badgeProxy = address(new ERC1967Proxy(badgeImpl, initData));
 
         // address badgeImpl = 0xe3c8a875D02F78C0E7F887599ad5e7fc2C33da02;
         // address badgeProxy = 0xf611ddA769F29BB0d76B2780647f224d89E5194D;
         // address renderer = 0xadd4a6bBb15B27dAb50fd3382a1C68F8BcE51cd1;
-        Badge(badgeProxy).transferOwnership(0xBb942519A1339992630b13c3252F04fCB09D4841);
+        // Badge(badgeProxy).transferOwnership(0xBb942519A1339992630b13c3252F04fCB09D4841);
+        vm.stopBroadcast();
     }
 }
