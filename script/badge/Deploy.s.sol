6a450edf41ae704c42e4a6d6c1095f2edafdeac4
Conner Swenberg <cls364@cornell.edu>
2023-05-15 13:15:46 -0400

    Deploy updates for 6551 demo

--- script/badge/Deploy.s.sol
+++ script/badge/Deploy.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "../../src/lib/renderer/Renderer.sol";
-import "../../src/membership/Membership.sol";
+import "../../src/badge/Badge.sol";
 import "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract Deploy is Script {
@@ -11,13 +11,16 @@ contract Deploy is Script {
 
     function run() public {
         vm.startBroadcast();
-        address owner = 0x016562aA41A8697720ce0943F003141f5dEAe006;
-        address renderer = 0x0DAb51C6d469001D31FfdE15Db9E539d8bAC4125;
-        address membershipImpl = address(new Membership());
 
-        bytes memory initData =
-            abi.encodeWithSelector(Membership(membershipImpl).initialize.selector, owner, renderer, "6551 Squad", "TBA");
-        new ERC1967Proxy(membershipImpl, initData);
+        address owner = 0x016562aA41A8697720ce0943F003141f5dEAe006;
+        address renderer = 0x0DAb51C6d469001D31FfdE15Db9E539d8bAC4125; // polygon
+        address badgeImpl = address(new Badge());
+
+        bytes memory initData = abi.encodeWithSelector(
+            Badge(badgeImpl).init.selector, owner, renderer, "Weekly Community Calls", "DEVCALLS"
+        );
+        address badgeProxy = address(new ERC1967Proxy(badgeImpl, initData));
+
         vm.stopBroadcast();
     }
 }
