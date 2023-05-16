b6cfd8b582e20740f9d82e451a15126ecfae6901
Conner Swenberg <cls364@cornell.edu>
2023-05-16 16:19:08 -0400

    Add OnePerAddress membership transfer test

--- test/guard/OnePerAddress.t.sol
+++ test/guard/OnePerAddress.t.sol
@@ -20,17 +20,18 @@ contract OnePerAddressTest is Test {
     function setUp() public {
         onePerAddress = address(new OnePerAddress());
         rendererImpl = address(new Renderer(msg.sender, "https://tokens.station.express"));
-        setUpMembership();
+        setUp_membership();
     }
 
-    function setUpMembership() public {
+    function setUp_membership() public {
         membershipImpl = address(new Membership());
         membershipFactory = new MembershipFactory(membershipImpl, msg.sender);
     }
 
-    function test_membership(address owner, address account) public {
+    function test_membershipMint(address owner, address account) public {
         vm.assume(owner != address(0));
         vm.assume(account != address(0));
+        vm.assume(account != address(this));
 
         address proxy = membershipFactory.create(owner, rendererImpl, "Test", "TEST");
         vm.startPrank(owner);
@@ -44,6 +45,31 @@ contract OnePerAddressTest is Test {
         Membership(proxy).mintTo(account);
         // balance still 1
         assertEq(Membership(proxy).balanceOf(account), 1);
+        vm.stopPrank();
+    }
+
+    function test_membershipTransfer(address owner, address account1, address account2) public {
+        vm.assume(owner != address(0));
+        vm.assume(account1 != address(0) && account1 != address(this));
+        vm.assume(account2 != address(0) && account2 != address(this));
+
+        address proxy = membershipFactory.create(owner, rendererImpl, "Test", "TEST");
+        vm.startPrank(owner);
+        // set guard
+        Permissions(proxy).guard(Permissions.Operation.TRANSFER, onePerAddress);
+        // mint to two addresses, should pass
+        uint256 token1 = Membership(proxy).mintTo(account1);
+        Membership(proxy).mintTo(account2);
+        assertEq(Membership(proxy).balanceOf(account1), 1);
+        assertEq(Membership(proxy).balanceOf(account2), 1);
+        vm.stopPrank();
+        vm.startPrank(account1);
+        // transfer 1->2, should fail
+        vm.expectRevert("NOT_ALLOWED");
+        Membership(proxy).transferFrom(account1, account2, token1);
+        // balances still 1
+        assertEq(Membership(proxy).balanceOf(account1), 1);
+        assertEq(Membership(proxy).balanceOf(account2), 1);
         vm.stopPrank();
     }
 }
