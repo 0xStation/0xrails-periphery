2d31cbdcc04663046fbcdd3f67b8a944f8ecf205
Conner Swenberg <cls364@cornell.edu>
2023-05-16 18:13:15 -0400

    Add small gas opt

--- src/lib/Batch.sol
+++ src/lib/Batch.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.13;
 /// @dev inspired by BoringBatchable: https://github.com/boringcrypto/BoringSolidity/blob/master/contracts/BoringBatchable.sol
 abstract contract Batch {
     function batch(bool atomic, bytes[] calldata calls) external payable {
-        for (uint256 i = 0; i < calls.length; i++) {
+        uint256 len = calls.length;
+        for (uint256 i = 0; i < len; i++) {
             (bool success,) = address(this).delegatecall(calls[i]);
             require(success || !atomic, "BATCH_FAIL");
         }
