78343fb76d41f70f7e4f8b7a24bb0dffd01729cc
Michael Gingras <mcg79@cornell.edu>
2023-05-30 18:29:53 -0600

    switching to setup/2

--- script/partners/EarlyDoors.s.sol
+++ script/partners/EarlyDoors.s.sol
@@ -14,41 +14,35 @@ contract EarlyDoors is Script {
     string public name = "Early Doors";
     string public symbol = "EARLYDOORS";
     uint8 public decimals = 2;
+    string public currency = "USD";
     uint256 public mintPrice = 300;
-    address public fakeToken = 0xD478219fDca296699A6511f28BA93a265E3E9a1b;
-    // REPLACE WITH USDC
+    address public usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant MAX_ADDRESS = 0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF;
+    // address public turnkey = 0xBb942519A1339992630b13c3252F04fCB09D4841;
 
-    // address public paymentCollector = 0x7d29e5eba46eeF74B6fC643aEc6300D5FC74F7B5; // early doors safe
-    address public paymentCollector = 0xE7affDB964178261Df49B86BFdBA78E9d768Db6D; // frog
+    address public paymentCollector = 0x7d29e5eba46eeF74B6fC643aEc6300D5FC74F7B5; // early doors safe
+    // address public paymentCollector = 0xE7affDB964178261Df49B86BFdBA78E9d768Db6D; // frog
 
     // address public owner = 0x7ff6363cd3A4E7f9ece98d78Dd3c862bacE2163d; // sym
     address public owner = 0xE7affDB964178261Df49B86BFdBA78E9d768Db6D; // frog
 
-    address public renderer = 0xf8A31352e237af670D5DC6e9b75a4401A37BaD0E; // goerli
+    // address public renderer = 0xf8A31352e237af670D5DC6e9b75a4401A37BaD0E; // goerli
     // address public renderer = 0x9AE8391F311292c8E241DB576C6d528932B1939f; // polygon
-    // address public renderer = 0xA9879cbfa6a1Fe2964F37BcCD6fcF6ea61EfcDbf; // mainnet
+    address public renderer = 0xA9879cbfa6a1Fe2964F37BcCD6fcF6ea61EfcDbf; // mainnet
 
-    // address public turnkey = 0xBb942519A1339992630b13c3252F04fCB09D4841;
-
-    address public onePerAddress = 0x8626BFA8dc92262d98A96A9a5CE8CCFDB0c59cB7; // goerli
+    // address public onePerAddress = 0x8626BFA8dc92262d98A96A9a5CE8CCFDB0c59cB7; // goerli
     // address public onePerAddress = 0xfD54A7a9E5df54872b07df99893CCD474C8f2b53; // polygon
-    // address public onePerAddress = 0x86dF40AC8ac8ec0ebAB4f42a88A75bAef3873649; // mainnet
+    address public onePerAddress = 0x86dF40AC8ac8ec0ebAB4f42a88A75bAef3873649; // mainnet
 
-    address public constant MAX_ADDRESS = 0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF;
-
-    address public membershipImpl = 0x0C461282106C3CD676091ebdAaA723Cd855fC1C2; // goerli
+    // address public membershipImpl = 0x0C461282106C3CD676091ebdAaA723Cd855fC1C2; // goerli
     // address public membershipImpl = 0xA9879cbfa6a1Fe2964F37BcCD6fcF6ea61EfcDbf; // polygon
-    // is this membership on mainnet the up to date version?
-    // address public membershipImpl = 0x629cB9eC3EF20624eb750E0670C1E2E81053Ab5A; // mainnet
-
-
+    address public membershipImpl = 0x629cB9eC3EF20624eb750E0670C1E2E81053Ab5A; // mainnet
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        // Membership membershipImpl = new Membership();
         Membership membership = Membership(membershipImpl);
 
         bytes memory initData =
@@ -56,13 +50,12 @@ contract EarlyDoors is Script {
         address proxy = address(new ERC1967Proxy(membershipImpl, initData));
 
         // purchase module
-        FixedStablecoinPurchaseModule purchaseModule = new FixedStablecoinPurchaseModule(owner, 0.001 ether, "USD", decimals);
-        purchaseModule.append(fakeToken);
-
+        FixedStablecoinPurchaseModule purchaseModule = new FixedStablecoinPurchaseModule(owner, 0.001 ether, currency, decimals);
+        purchaseModule.append(usdcAddress);
 
         // config
         address[] memory enabledTokens = new address[](1);
-        enabledTokens[0] = fakeToken;
+        enabledTokens[0] = usdcAddress;
         bytes memory setupModuleData = abi.encodeWithSelector(
             bytes4(keccak256("setup(uint256,bytes32)")), mintPrice * 10**decimals, purchaseModule.enabledTokensValue(enabledTokens)
         );
