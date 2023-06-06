8a0247b8a9d71c79816a4479fcfe35284996dfec
pascal <pascal>
2023-06-06 09:58:03 -0400

    feat: add presets feature

--- src/membership/storage/MembershipFactoryStorageV0.sol
+++ src/membership/storage/MembershipFactoryStorageV0.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.13;
 
 abstract contract MembershipFactoryStorageV0 {
+    struct Preset {
+        string desc;
+        bytes[] calls;
+    }
+
     address public template;
+    Preset[] public presets;
 }
