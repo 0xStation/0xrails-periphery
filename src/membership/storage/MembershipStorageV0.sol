12aacbed12db8a35ca2f22f81b3c99ae50453875
robriks <marsterlund@gmail.com>
2023-07-10 17:24:13 -0400

    :memo: Typo fixes

--- src/membership/storage/MembershipStorageV0.sol
+++ src/membership/storage/MembershipStorageV0.sol
@@ -8,6 +8,6 @@ abstract contract MembershipStorageV0 is IMembership {
     address public renderer;
     // self-incrementing id for minting tokens, doubly functions as totalSupply function
     uint256 public totalSupply;
-    // address of the payment collector, receieves all payments from minting events
+    // address of the payment collector, receives all payments from minting events
     address public paymentCollector;
 }
