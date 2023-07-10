4e2fa13f5b2c172bddf063e258c5ad742eb11cc1
robriks <marsterlund@gmail.com>
2023-07-10 16:42:58 -0400

    first draft of ModuleFeeV2:ETH, minimal changes

--- src/lib/module/ModuleFeeV2.sol
+++ src/lib/module/ModuleFeeV2.sol
@@ -2,30 +2,54 @@
 pragma solidity ^0.8.13;
 
 import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {FeeManager} from "./FeeManager.sol";
 
-contract ModuleFee is Ownable {
-    uint256 public fee;
-    uint256 public feeBalance;
+/// @title Station Network Fee Manager Contract
+/// @author 👦🏻👦🏻.eth
+
+/// @dev This contract enables payment by handling funds when charging base and variable fees on each Membership's mints
+/// @dev This module should be inherited by all Membership collections that intend to accept payment in either ETH or ERC20 tokens
+
+/// @notice todo This implementation currently only handles ETH, will need to be mixed with (storage-packed) ERC20 logic from FixedStablecoinPurchaseModule.sol
+contract ModuleFeeV2 is Ownable {
+
+    /*=============
+        STORAGE
+    =============*/
+
+    /// @dev Address of the deployed FeeManager contract which stores state for all collections' fee information
+    /// @dev The FeeManger serves a Singleton role as central fee ledger for modules to read from
+    address immutable internal feeManager;
+
+    /// @dev The balance recordkeeping for the specific child contract that inherits from this module
+    /// @dev Accounts for the sum of both baseFee and variableFee, which are set in the FeeManager
+    uint256 public totalFeeBalance;
+
+    /*============
+        ERRORS
+    ============*/
 
     error InvalidFee(uint256 expected, uint256 received);
 
-    event UpdateFee(uint256 fee);
+    /*============
+        EVENTS
+    ============*/
+
     event WithdrawFee(address indexed recipient, uint256 amount);
 
-    constructor(address newOwner, uint256 newFee) {
+    /*=================
+        ModuleFeeV2
+    =================*/
+
+    /// @param newOwner The initialization of the contract's owner address, managed by Station
+    /// @param feeManagerProxy The UUPS proxy that serves as Station's central fee management ledger for all Memberships
+    constructor(address newOwner, address feeManagerProxy) {
         _transferOwnership(newOwner);
-        _updateFee(newFee);
+        feeManager = feeManagerProxy;
     }
 
-    function updateFee(uint256 newFee) external onlyOwner {
-        _updateFee(newFee);
-    }
-
-    function _updateFee(uint256 newFee) internal {
-        fee = newFee;
-        emit UpdateFee(newFee);
-    }
-
+    /// @dev Function to withdraw the total balances of accrued base and variable fees that have been collected from mints
+    /// @dev Sends fees to the module's owner address, which is managed by Station Network
     function withdrawFee() external {
         address recipient = owner();
         uint256 balance = feeBalance;
@@ -34,13 +58,19 @@ contract ModuleFee is Ownable {
         emit WithdrawFee(recipient, balance);
     }
 
+    /// @dev Function to update the feeBalance in storage when minting a single item
     function _registerFee() internal returns (uint256 paidFee) {
         return _registerFeeBatch(1);
     }
 
+    /// @dev Function to update the feeBalance in storage when fees are paid to this module
+    /// @param n The number of items being minted, used to calculate the total fee payment required
     function _registerFeeBatch(uint256 n) internal returns (uint256 paidFee) {
-        paidFee = fee * n; // read from state once, gas optimization
+        // formerly: paidFee = fee * n; // read from state once, gas optimization
+        // todo: FeeManager(feeManager).getFeeTotals();
+        // accept funds only if the msg.value sent matches the FeeManager's calculation
         if (paidFee != msg.value) revert InvalidFee(paidFee, msg.value);
+        // update balances
         feeBalance += paidFee;
     }
 }
