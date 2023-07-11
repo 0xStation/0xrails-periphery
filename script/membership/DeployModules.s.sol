41f737f724ec5496dc6d59ee24c6fbc1211f076e
Conner Swenberg <cls364@cornell.edu>
2023-07-11 14:55:00 -0700

    Add new deployments for V2 modules

--- script/membership/DeployModules.s.sol
+++ script/membership/DeployModules.s.sol
@@ -8,14 +8,15 @@ import {FreeMintModuleV2} from "src/membership/modules/FreeMintModuleV2.sol";
 
 // forge script script/modules/DeployFixedETHPurchaseModule.s.sol:Deploy --fork-url $GOERLI_RPC_URL --keystores $ETH_KEYSTORE --password $KEYSTORE_PASSWORD --sender $ETH_FROM --broadcast
 // forge verify-contract 0x928d70acd89cc4d18f7ac9d28cf77646ea42bd4a ./src/modules/FixedETHPurchaseModule.sol:FixedETHPurchaseModule $ETHERSCAN_API_KEY --chain-id 5
-contract Deploy is Script {
+contract DeployModules is Script {
     address owner = 0x016562aA41A8697720ce0943F003141f5dEAe006; // sym
 
     uint256 fee = 0.0001 ether; // ethereum
     // uint256 fee = 2 ether; // polygon
 
-    address USDC = 0x016562aA41A8697720ce0943F003141f5dEAe006; // goerli
-    address DAI = 0x016562aA41A8697720ce0943F003141f5dEAe006; // goerli
+    address FAKE = 0xD478219fDca296699A6511f28BA93a265E3E9a1b; // goerli
+    // address USDC = 0x016562aA41A8697720ce0943F003141f5dEAe006; // goerli
+    // address DAI = 0x016562aA41A8697720ce0943F003141f5dEAe006; // goerli
 
     function run() public {
         vm.startBroadcast();
@@ -24,8 +25,7 @@ contract Deploy is Script {
         new EthPurchaseModuleV2(owner, fee);
 
         address[] memory stablecoins = new address[](1);
-        stablecoins[0] = USDC;
-        stablecoins[0] = DAI;
+        stablecoins[0] = FAKE;
         new StablecoinPurchaseModuleV2(owner, fee, 2, "USD", stablecoins);
 
         vm.stopBroadcast();
