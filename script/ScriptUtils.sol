59b16c540b4dee834ebd56f431b4add65843e4aa
robriks <marsterlund@gmail.com>
2023-09-14 16:52:06 -0400

    :sparkles: Update ScriptUtils using the one in 0xRails

--- script/ScriptUtils.sol
+++ script/ScriptUtils.sol
@@ -1,21 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Script} from "forge-std/Script.sol";
 
-contract ScriptUtils {
+abstract contract ScriptUtils is Script {
+    error Create2Failure();
+
     // global addresses
     address public constant MAX_ADDRESS = 0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF;
+    // valid as of 09/14/23
     address public constant turnkey = 0xBb942519A1339992630b13c3252F04fCB09D4841;
-    // address public constant stationBot = ___;
 
     // dev addresses
     address public constant symmetry = 0x7ff6363cd3A4E7f9ece98d78Dd3c862bacE2163d;
     address public constant frog = 0xE7affDB964178261Df49B86BFdBA78E9d768Db6D;
-    // address public constant robriks = ___;
+    address public constant paprika = 0x4b8c47aE2e5083EE6AA9aE2884E8051c2e4741b1;
+    address public constant robriks = 0xFFFFfFfFA2eC6F66a22017a0Deb0191e5F8cBc35;
 
-    // to avoid collisions of a commonly deployed contract, create a combined salt with the other arguments
-    function deployERC1967Proxy(bytes32 salt, address implementation, bytes memory initCode) public returns (address proxy) {
-        return address(new ERC1967Proxy{salt: keccak256(abi.encodePacked(salt, implementation, initCode))}(implementation, initCode));
+    // reads a plain extensionless file containing *only the salt string* 
+    function readSalt(string memory fileName) internal view returns (string memory) {
+        string memory inputDir = "./script/input/";
+        string memory file = string.concat(inputDir, fileName);
+        return vm.readFile(file);
+    }
+
+    // write used salts to an output file upon completion of scripts that used `readSalt()`
+    function writeUsedSalt(string memory consumedSalt, string memory deployment) internal {
+        // eg. "GarlicSalt : ERC721Mage, "
+        string memory output = string.concat(consumedSalt, " : ", deployment, ", ");
+        string memory dest = "./script/input/usedSalts";
+        return vm.writeLine(dest, output);
     }
 }
