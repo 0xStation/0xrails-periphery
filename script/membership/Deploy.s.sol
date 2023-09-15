6f8cfe467952cd573aaee0e14bc4de968a212955
Markus Osterlund / robriks <80549215+robriks@users.noreply.github.com>
2023-09-15 15:11:11 -0400

    Update deploy scripts (#49)
    * :sparkles: Update ScriptUtils using the one in 0xRails
    
    * :sparkles: Add script/input dir, gitignore it, configure .toml to allow read/write to it only, move ScriptUtils to new script/utils dir
    
    * :rocket: Update Deploy script to use create2 with ScriptUtils salt filereading
    
    * :sparkles: Write used salts for each deployment to script/input/usedSalts

--- script/membership/Deploy.s.sol
+++ script/membership/Deploy.s.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script} from "forge-std/Script.sol";
-import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
-
+import {ScriptUtils} from "script/utils/ScriptUtils.sol";
 import {FeeManager} from "../../src/lib/module/FeeManager.sol";
 import {FreeMintModule} from "../../src/membership/modules/FreeMintModule.sol";
 import {GasCoinPurchaseModule} from "../../src/membership/modules/GasCoinPurchaseModule.sol";
@@ -14,39 +12,38 @@ import {NFTMetadataRouterExtension} from
     "../../src/membership/extensions/NFTMetadataRouter/NFTMetadataRouterExtension.sol";
 import {PayoutAddressExtension} from "../../src/membership/extensions/PayoutAddress/PayoutAddressExtension.sol";
 import {MembershipFactory} from "../../src/membership/factory/MembershipFactory.sol";
+import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
 
-contract Deploy is Script {
-    address public turnkey = 0xBb942519A1339992630b13c3252F04fCB09D4841;
-    address public frog = 0xE7affDB964178261Df49B86BFdBA78E9d768Db6D;
-    address public sym = 0x7ff6363cd3A4E7f9ece98d78Dd3c862bacE2163d;
+contract Deploy is ScriptUtils {
 
-    address public owner = sym;
+        /*=================
+            ENVIRONMENT 
+        =================*/
 
-    function setUp() public {}
+        // The following contracts will be deployed:
+        MetadataRouter metadataRouterImpl;
+        MetadataRouter metadataRouter; // proxy
+        OnePerAddressGuard onePerAddressGuard;
+        NFTMetadataRouterExtension nftMetadataRouterExtension;
+        PayoutAddressExtension payoutAddressExtension;
+        FeeManager feeManager;
+        FreeMintModule freeMintModule;
+        GasCoinPurchaseModule gasCoinPurchaseModule;
+        StablecoinPurchaseModule stablecoinPurchaseModule;
+        MembershipFactory membershipFactoryImpl;
+        MembershipFactory membershipFactory; // proxy
+
+        // uncomment all instances of `deployerPrivateKey` if using a private key in shell env var
+        // uint256 deployerPrivateKey = vm.envUint("PK");
 
     function run() public {
-        vm.startBroadcast();
 
-        /// @dev first deploy ERC721Rails from the 0xrails repo and update the address in `deployMembershipFactory`!
+        /*============
+            CONFIG
+        ============*/
 
-        address metadataRouter = deployMetadataRouter();
-        deployOnePerAddressGuard(metadataRouter);
-        deployNFTMetadataRouterExtension(metadataRouter);
-        deployPayoutAddressExtension(metadataRouter);
-
-        address feeManager = deployFeeManager();
-        deployFreeMintModule(feeManager, metadataRouter);
-        deployGasCoinPurchaseModule(feeManager, metadataRouter);
-        deployStablecoinPurchaseModule(feeManager, metadataRouter);
-
-        deployMembershipFactory();
-
-        // missing: ExtensionBeacon
-
-        vm.stopBroadcast();
-    }
-
-    function deployMetadataRouter() internal returns (address) {
+        // `deployMetadataRouter()` params configuration
         // string memory defaultURI = "https://groupos.xyz/api/v1/contractMetadata";
         string memory defaultURI = "https://dev.groupos.xyz/api/v1/contractMetadata"; // goerli
         string[] memory routes = new string[](1);
@@ -55,44 +52,7 @@ contract Deploy is Script {
         // uris[0] = "https://groupos.xyz/api/v1/nftMetadata";
         uris[0] = "https://dev.groupos.xyz/api/v1/nftMetadata"; // goerli
 
-        address metadataRouterImpl = address(new MetadataRouter());
-
-        bytes memory initData = abi.encodeWithSelector(
-            MetadataRouter(metadataRouterImpl).initialize.selector, owner, defaultURI, routes, uris
-        );
-        return address(new ERC1967Proxy(metadataRouterImpl, initData));
-    }
-
-    function deployOnePerAddressGuard(address metadataRouter) internal returns (address) {
-        return address(new OnePerAddressGuard(metadataRouter));
-    }
-
-    function deployNFTMetadataRouterExtension(address metadataRouter) internal returns (address) {
-        return address(new NFTMetadataRouterExtension(metadataRouter));
-    }
-
-    function deployPayoutAddressExtension(address metadataRouter) internal returns (address) {
-        return address(new PayoutAddressExtension(metadataRouter));
-    }
-
-    function deployFeeManager() internal returns (address) {
-        uint120 ethBaseFee = 1e15; // 0.001 ETH
-        // uint120 polygonBaseFee = 2e18; // 2 MATIC
-        uint120 defaultBaseFee = 0;
-        uint120 defaultVariableFee = 500; // 5%
-
-        return address(new FeeManager(owner, defaultBaseFee, defaultVariableFee, ethBaseFee, defaultVariableFee));
-    }
-
-    function deployFreeMintModule(address feeManager, address metadataRouter) internal returns (address) {
-        return address(new FreeMintModule(owner, feeManager, metadataRouter));
-    }
-
-    function deployGasCoinPurchaseModule(address feeManager, address metadataRouter) internal returns (address) {
-        return address(new GasCoinPurchaseModule(owner, feeManager, metadataRouter));
-    }
-
-    function deployStablecoinPurchaseModule(address feeManager, address metadataRouter) internal returns (address) {
+        // `deployStablecoinPurchaseModule` params configuration
         uint8 decimals = 2;
         string memory currency = "USD";
         address[] memory stablecoins = new address[](1);
@@ -100,15 +60,96 @@ contract Deploy is Script {
         // stablecoins[0] = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174; // polygon
         // stablecoins[0] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // ethereum
 
-        return address(new StablecoinPurchaseModule(owner, feeManager, decimals, currency, stablecoins, metadataRouter));
+        /*===============
+            BROADCAST 
+        ===============*/
+
+        vm.startBroadcast(/*deployerPrivateKey*/);
+
+        address owner = ScriptUtils.symmetry;
+        string memory saltString = ScriptUtils.readSalt("salt");
+        bytes32 salt = bytes32(bytes(saltString));
+        
+        // eventually we can just use ScriptUtils to read from deploys.json
+        address erc721Rails = 0x7c804b088109C23d9129366a8C069448A4b219F8; // goerli, polygon, mainnet
+
+        (metadataRouterImpl, metadataRouter) = deployMetadataRouter(owner, defaultURI, routes, uris, salt);
+        onePerAddressGuard = deployOnePerAddressGuard(address(metadataRouter), salt);
+        nftMetadataRouterExtension = deployNFTMetadataRouterExtension(address(metadataRouter), salt);
+        payoutAddressExtension = deployPayoutAddressExtension(address(metadataRouter), salt);
+
+        feeManager = FeeManager(deployFeeManager(owner, salt));
+        freeMintModule = deployFreeMintModule(owner, address(feeManager), address(metadataRouter), salt);
+        gasCoinPurchaseModule = deployGasCoinPurchaseModule(owner, address(feeManager), address(metadataRouter), salt);
+
+        // using stablecoin 'environment' params above
+        stablecoinPurchaseModule = deployStablecoinPurchaseModule(owner, address(feeManager), decimals, currency, stablecoins, address(metadataRouter), salt);
+
+        (membershipFactoryImpl, membershipFactory) = deployMembershipFactory(owner, erc721Rails, salt);
+
+        // missing: ExtensionBeacon
+
+        vm.stopBroadcast();
+
+        writeUsedSalt(saltString, string.concat("MetaDataRouterImpl @", Strings.toHexString(address(metadataRouterImpl))));
+        writeUsedSalt(saltString, string.concat("MetaDataRouterProxy @", Strings.toHexString(address(metadataRouter))));
+        writeUsedSalt(saltString, string.concat("NFTMetadataRouterExtension @", Strings.toHexString(address(nftMetadataRouterExtension))));
+        writeUsedSalt(saltString, string.concat("PayoutAddressExtension @", Strings.toHexString(address(payoutAddressExtension))));
+        writeUsedSalt(saltString, string.concat("FeeManager @", Strings.toHexString(address(feeManager))));
+        writeUsedSalt(saltString, string.concat("FreeMintModule @", Strings.toHexString(address(freeMintModule))));
+        writeUsedSalt(saltString, string.concat("GasCoinPurchaseModule @", Strings.toHexString(address(gasCoinPurchaseModule))));
+        writeUsedSalt(saltString, string.concat("StablecoinPurchaseModule @", Strings.toHexString(address(stablecoinPurchaseModule))));
+        writeUsedSalt(saltString, string.concat("MembershipFactoryImpl @", Strings.toHexString(address(membershipFactoryImpl))));
+        writeUsedSalt(saltString, string.concat("MembershipFactoryProxy @", Strings.toHexString(address(membershipFactory))));
     }
 
-    function deployMembershipFactory() internal returns (address) {
-        address erc721Rails = 0x7c804b088109C23d9129366a8C069448A4b219F8; // goerli, polygon, mainnet
-        address membershipFactoryImpl = address(new MembershipFactory());
+    function deployMetadataRouter(address _owner, string memory _defaultURI, string[] memory _routes, string[] memory _uris, bytes32 _salt) internal returns (MetadataRouter _impl, MetadataRouter _proxy) {
+        _impl = new MetadataRouter{salt: _salt}();
+
+        bytes memory initData = abi.encodeWithSelector(
+            MetadataRouter.initialize.selector, _owner, _defaultURI, _routes, _uris
+        );
+        _proxy = MetadataRouter(address(new ERC1967Proxy{salt: _salt}(address(_impl), initData)));
+    }
+
+    function deployOnePerAddressGuard(address _metadataRouter, bytes32 _salt) internal returns (OnePerAddressGuard) {
+        return new OnePerAddressGuard{salt: _salt}(_metadataRouter);
+    }
+
+    function deployNFTMetadataRouterExtension(address _metadataRouter, bytes32 _salt) internal returns (NFTMetadataRouterExtension) {
+        return new NFTMetadataRouterExtension{salt: _salt}(_metadataRouter);
+    }
+
+    function deployPayoutAddressExtension(address _metadataRouter, bytes32 _salt) internal returns (PayoutAddressExtension) {
+        return new PayoutAddressExtension{salt: _salt}(_metadataRouter);
+    }
+
+    function deployFeeManager(address _owner, bytes32 _salt) internal returns (FeeManager) {
+        uint120 _ethBaseFee = 1e15; // 0.001 ETH
+        // uint120 polygonBaseFee = 2e18; // 2 MATIC
+        uint120 _defaultBaseFee = 0;
+        uint120 _defaultVariableFee = 500; // 5%
+
+        return new FeeManager{salt: _salt}(_owner, _defaultBaseFee, _defaultVariableFee, _ethBaseFee, _defaultVariableFee);
+    }
+
+    function deployFreeMintModule(address _owner, address _feeManager, address _metadataRouter, bytes32 _salt) internal returns (FreeMintModule) {
+        return new FreeMintModule{salt: _salt}(_owner, _feeManager, _metadataRouter);
+    }
+
+    function deployGasCoinPurchaseModule(address _owner, address _feeManager, address _metadataRouter, bytes32 _salt) internal returns (GasCoinPurchaseModule) {
+        return new GasCoinPurchaseModule{salt: _salt}(_owner, _feeManager, _metadataRouter);
+    }
+
+    function deployStablecoinPurchaseModule(address _owner, address _feeManager, uint8 _decimals, string memory _currency, address[] memory _stablecoins, address _metadataRouter, bytes32 _salt) internal returns (StablecoinPurchaseModule) {
+        return new StablecoinPurchaseModule{salt: _salt}(_owner, _feeManager, _decimals, _currency, _stablecoins, _metadataRouter);
+    }
+
+    function deployMembershipFactory(address _owner, address _erc721Rails, bytes32 _salt) internal returns (MembershipFactory _impl, MembershipFactory _proxy) {
+        _impl = new MembershipFactory{salt: _salt}();
 
         bytes memory initFactory =
-            abi.encodeWithSelector(MembershipFactory(membershipFactoryImpl).initialize.selector, erc721Rails, owner);
-        return address(new ERC1967Proxy(membershipFactoryImpl, initFactory));
+            abi.encodeWithSelector(MembershipFactory.initialize.selector, _erc721Rails, _owner);
+        _proxy = MembershipFactory(address(new ERC1967Proxy{salt: _salt}(address(_impl), initFactory)));
     }
 }
