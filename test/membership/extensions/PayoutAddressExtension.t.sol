1ac688dbf42f86edd801e4012a6a4500c55c2f44
robriks <marsterlund@gmail.com>
2023-09-15 16:18:43 -0400

    :white_check_mark: WIP PayoutAddressExtension testing

--- test/membership/extensions/PayoutAddressExtension.t.sol
+++ test/membership/extensions/PayoutAddressExtension.t.sol
@@ -2,19 +2,23 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
+import {MockAccountDeployer} from "lib/0xrails/test/lib/MockAccount.sol";
+import {Operations} from "0xrails/lib/Operations.sol";
 import {PayoutAddressExtension} from "src/membership/extensions/PayoutAddress/PayoutAddressExtension.sol";
+import {PayoutAddress} from "src/membership/extensions/PayoutAddress/PayoutAddress.sol";
 import {MetadataRouter} from "src/metadataRouter/MetadataRouter.sol";
 import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract PayoutAddressExtensionTest is Test {
+contract PayoutAddressExtensionTest is Test, MockAccountDeployer {
 
     PayoutAddressExtension public payoutAddressExtension;
     MetadataRouter public exampleRouterImpl;
     MetadataRouter public exampleRouterProxy;
 
     address public owner;
-    string public someURI;
     string public someRoute;
+    string public defaultURI;
+    string public someURI;
     string[] public routes;
     string[] public uris;
     bytes public initData;
@@ -27,16 +31,19 @@ contract PayoutAddressExtensionTest is Test {
         someRoute = "token";
         routes = new string[](1);
         routes[0] = someRoute;
+        defaultURI = "default";
         someURI = "someURI";
         uris = new string[](1);
         uris[0] = someURI;
         initData = abi.encodeWithSelector(
-            MetadataRouter.initialize.selector, owner, someURI, routes, uris
+            MetadataRouter.initialize.selector, owner, defaultURI, routes, uris
         );
 
         exampleRouterImpl = new MetadataRouter();
         exampleRouterProxy = MetadataRouter(address(new ERC1967Proxy(address(exampleRouterImpl), initData)));
 
+        // deploy payoutAddressExtension as `owner` so it grants `owner` ADMIN permission
+        vm.prank(owner);
         payoutAddressExtension = new PayoutAddressExtension(address(exampleRouterProxy));
     }
 
@@ -44,10 +51,43 @@ contract PayoutAddressExtensionTest is Test {
         string memory returnedURI = exampleRouterProxy.routeURI(someRoute);
         string memory expectedURI = someURI;
         assertEq(returnedURI, expectedURI);
+        
+        string memory returnedDefault = exampleRouterProxy.defaultURI();
+        string memory expectedDefault = defaultURI;
+        assertEq(returnedDefault, expectedDefault);
     }
 
-    function test_getAllSelectors() public {}
-    function test_signatureOf() public {}
-    function test_updatePayoutAddress() public {}
-    function test_removePayoutAddress() public {}
+    function test_getAllSelectors() public {
+        bytes4[] memory selectors = payoutAddressExtension.getAllSelectors();
+        assertEq(selectors.length, 3);
+        assertEq(selectors[0], PayoutAddress.payoutAddress.selector);
+        assertEq(selectors[1], PayoutAddress.updatePayoutAddress.selector);
+        assertEq(selectors[2], PayoutAddress.removePayoutAddress.selector);
+    }
+
+    function test_signatureOf() public {
+        bytes4[] memory selectors = payoutAddressExtension.getAllSelectors();
+        string memory returnedPayoutAddressSignature = payoutAddressExtension.signatureOf(selectors[0]);
+        string memory expectedPayoutAddressSignature = "payoutAddress()";
+        assertEq(returnedPayoutAddressSignature, expectedPayoutAddressSignature);
+
+        string memory updatePayoutSignature = payoutAddressExtension.signatureOf(selectors[1]);
+        string memory expectedUpdatePayoutSignature = "updatePayoutAddress(address)";
+        assertEq(updatePayoutSignature, expectedUpdatePayoutSignature);
+
+        string memory removePayoutSignature = payoutAddressExtension.signatureOf(selectors[2]);
+        string memory expectedRemovePayoutSignature = "removePayoutAddress()";
+        assertEq(removePayoutSignature, expectedRemovePayoutSignature);
+    }
+
+    function test_updatePayoutAddress() public {
+        address newPayoutAddress = createAccount();
+
+        vm.prank(owner);
+        payoutAddressExtension.updatePayoutAddress(newPayoutAddress);
+
+        assertEq(payoutAddressExtension.payoutAddress(), newPayoutAddress);
+    }
+
+
 }
