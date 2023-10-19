b56bca6ec8f7cd3ae9ed6011b791a5c791c19ad2
Markus Osterlund / robriks <80549215+robriks@users.noreply.github.com>
2023-10-19 16:28:13 -0400

    Accountgroup deploy script (#71)
    * :rocket: Update accountgroup deploy script for production
    
    * :rocket: Update deployment script with IPermissions selectors
    
    * :arrow_up: Update protocol-ops dependency
    
    * :arrow_up: Fix submodules issue
    
    * :rocket: AccountGroup Linea mainnet+testnet deployment commit

--- script/accountGroup/AccountGroup.s.sol
+++ script/accountGroup/AccountGroup.s.sol
@@ -5,6 +5,7 @@ import {ScriptUtils} from "protocol-ops/script/ScriptUtils.sol";
 import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
 import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {Operations} from "0xrails/lib/Operations.sol";
+import {IPermissions} from "0xrails/access/permissions/Permissions.sol";
 
 import {AccountGroup} from "../../src/accountGroup/implementation/AccountGroup.sol";
 import {PermissionGatedInitializer} from "../../src/accountGroup/initializer/PermissionGatedInitializer.sol";
@@ -28,10 +29,9 @@ contract AccountGroupScript is ScriptUtils {
 
         vm.startBroadcast();
 
-        // address owner = ScriptUtils.stationFounderSafe;
-        address owner = ScriptUtils.symmetry;
+        address owner = ScriptUtils.stationFounderSafe;
 
-        string memory saltString = ScriptUtils.readSalt("salt");
+        string memory saltString = "station";
         bytes32 salt = bytes32(bytes(saltString));
 
         // begin deployments
@@ -40,34 +40,50 @@ contract AccountGroupScript is ScriptUtils {
         initializeAccountController = new InitializeAccountController{salt: salt}();
 
         // After deployments, format Multicall3 calls and execute it from FounderSafe as module sender
-        // `MetadataRouter::setDefaultURI()` configuration
 
         // LOCAL DEPLOY
         
-        accountGroup.setDefaultAccountInitializer(address(permissionGatedInitializer));
-        accountGroup.addPermission(Operations.INITIALIZE_ACCOUNT, address(initializeAccountController));
-        accountGroup.addPermission(Operations.INITIALIZE_ACCOUNT_PERMIT, ScriptUtils.turnkey);
+        // accountGroup.setDefaultAccountInitializer(address(permissionGatedInitializer));
+        // accountGroup.addPermission(Operations.INITIALIZE_ACCOUNT, address(initializeAccountController));
+        // accountGroup.addPermission(Operations.INITIALIZE_ACCOUNT_PERMIT, ScriptUtils.turnkey);
 
         // PRODUCTION DEPLOY 
 
-        // bytes memory setDefaultAccountInitializer = abi.encodeWithSelector(
-        //     AccountGroup.setDefaultAccountInitializer.selector, address(permissionGatedInitializer)
-        // );
-        // Call3 memory accountGroupSetDefaultAccountInitializerCall =
-        //     Call3({target: address(accountGroup), allowFailure: false, callData: setDefaultAccountInitializer});
+        bytes memory setDefaultAccountInitializer = abi.encodeWithSelector(
+            AccountGroup.setDefaultAccountInitializer.selector, address(permissionGatedInitializer)
+        );
+        Call3 memory accountGroupSetDefaultAccountInitializerCall =
+            Call3({target: address(accountGroup), allowFailure: false, callData: setDefaultAccountInitializer});
 
-        // Call3[] memory calls = new Call3[](1);
-        // calls[0] = accountGroupSetDefaultAccountInitializerCall;
-        // bytes memory multicallData = abi.encodeWithSignature("aggregate3((address,bool,bytes)[])", calls);
-        // // `Safe(owner).execTransactionFromModule(multicall3, 0, multicallData, uint8(1));` using 0 ETH value & Operation == DELEGATECALL
-        // bytes memory safeCall = abi.encodeWithSignature(
-        //     "execTransactionFromModule(address,uint256,bytes,uint8)", multicall3, 0, multicallData, uint8(1)
-        // );
-        // (bool r,) = owner.call(safeCall);
-        // require(r);
+        bytes memory addPermissionInitializeAccountToController = abi.encodeWithSelector(
+            IPermissions.addPermission.selector, Operations.INITIALIZE_ACCOUNT, address(initializeAccountController)
+        );
+        Call3 memory addPermissionInitializeAccountToControllerCall = 
+            Call3({target: address(accountGroup), allowFailure: false, callData: addPermissionInitializeAccountToController});
+
+        bytes memory addPermissionInitializeAccountPermitToTurnkey = abi.encodeWithSelector(
+            IPermissions.addPermission.selector, Operations.INITIALIZE_ACCOUNT_PERMIT, ScriptUtils.turnkey
+        );
+        Call3 memory addPermissionInitializeAccountPermitToTurnkeyCall = 
+            Call3({target: address(accountGroup), allowFailure: false, callData: addPermissionInitializeAccountPermitToTurnkey});
+
+        Call3[] memory calls = new Call3[](3);
+        calls[0] = accountGroupSetDefaultAccountInitializerCall;
+        calls[1] = addPermissionInitializeAccountToControllerCall;
+        calls[2] = addPermissionInitializeAccountPermitToTurnkeyCall;
+
+        bytes memory multicallData = abi.encodeWithSignature("aggregate3((address,bool,bytes)[])", calls);
+        // `Safe(owner).execTransactionFromModule(multicall3, 0, multicallData, uint8(1));` using 0 ETH value & Operation == DELEGATECALL
+        bytes memory safeCall = abi.encodeWithSignature(
+            "execTransactionFromModule(address,uint256,bytes,uint8)", multicall3, 0, multicallData, uint8(1)
+        );
+        (bool r,) = owner.call(safeCall);
+        require(r);
 
         // assert accountGroup call successful
         assert(accountGroup.getDefaultAccountInitializer() == address(permissionGatedInitializer));
+        assert(accountGroup.hasPermission(Operations.INITIALIZE_ACCOUNT, address(initializeAccountController)));
+        assert(accountGroup.hasPermission(Operations.INITIALIZE_ACCOUNT_PERMIT, ScriptUtils.turnkey));
 
         vm.stopBroadcast();
 
