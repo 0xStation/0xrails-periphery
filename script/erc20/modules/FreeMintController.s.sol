b798ccc204222b1dd79cff908ab0dcbc7ba182fe
robriks <marsterlund@gmail.com>
2023-10-23 16:47:53 -0400

    :rocket: Update FreeMintController deployment script

--- script/erc20/modules/FreeMintController.s.sol
+++ script/erc20/modules/FreeMintController.s.sol
@@ -4,24 +4,30 @@ pragma solidity ^0.8.13;
 import {ScriptUtils} from "lib/protocol-ops/script/ScriptUtils.sol";
 import {ITokenFactory} from "src/factory/ITokenFactory.sol";
 import {FreeMintController} from "../../../src/erc20/modules/FreeMintController.sol";
-
+import {console2} from "forge-std/console2.sol";
 
 // forge script --keystores $ETH_KEYSTORE --sender $ETH_FROM --broadcast --fork-url $GOERLI_RPC_URL script/erc20/modules/FreeMintController.s.sol
-// deployed FreeMintController address: 0x8E019DfdA444743CA58065bd9b24Bd569b61fa75
+// See deployed FreeMintController address in `Protocol-Ops::deploys.json`
 contract DeployERC20FreeMintModule is ScriptUtils {
+
+    /*============
+        CONFIG
+    ============*/
+
+    // following contract will be deployed
+    FreeMintController erc20FreeMintModule;
+
+    /*===============
+        BROADCAST 
+    ===============*/
 
     function run() public {
         vm.startBroadcast();
-        // deploy FreeMintController
-        // address _owner = 0xE7affDB964178261Df49B86BFdBA78E9d768Db6D;
-        // address _feeManager = 0x4B5f9B012842d449f84c93B1b9c85A730ea7cd7A;
-        // address _metadataRouter = 0xeB9359fB4154e4EF9B3Ba153aA86555F69Cd2E3E;
-        // new FreeMintController(_owner, _feeManager, _metadataRouter);
 
-        // add collection to FreeMintController
-        address sym2Collection = 0x8D007613435453041ec6d03E87a90117507065D0;
-        FreeMintController fmc = FreeMintController(0x8E019DfdA444743CA58065bd9b24Bd569b61fa75);
-        fmc.setUp(sym2Collection, true);
+        string memory saltString = "station";
+        bytes32 salt = bytes32(bytes(saltString));
+
+        new FreeMintController{salt: salt}();
 
         vm.stopBroadcast();
     }
