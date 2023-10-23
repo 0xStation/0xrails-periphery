2d5e71b76957232921782ea159d41fdce0500470
robriks <marsterlund@gmail.com>
2023-10-23 16:46:03 -0400

    :rocket: ERC20 deployment script

--- script/token/CreateERC20.s.sol
+++ script/token/CreateERC20.s.sol
@@ -5,30 +5,24 @@ import {ScriptUtils} from "lib/protocol-ops/script/ScriptUtils.sol";
 import {Multicall} from "openzeppelin-contracts/utils/Multicall.sol";
 import {Permissions} from "0xrails/access/permissions/Permissions.sol";
 import {Operations} from "0xrails/lib/Operations.sol";
-import {ITokenFactory} from "src/factory/ITokenFactory.sol";
-import {FreeMintController} from "../../src/membership/modules/FreeMintController.sol";
-import {GasCoinPurchaseController} from "../../src/membership/modules/GasCoinPurchaseController.sol";
-import {StablecoinPurchaseController} from "../../src/membership/modules/StablecoinPurchaseController.sol";
-import {TokenFactory} from "../../src/factory/TokenFactory.sol";
+import {TokenFactory} from "src/factory/TokenFactory.sol";
+// import {console2} from "forge-std/console2.sol";
 
 contract CreateERC20 is ScriptUtils {
     /*============
         CONFIG
     ============*/
 
-    /// @notice LINEA: v.1.10
-    // address coreImpl = 0xe0dd2F320290d04Dce5432E6ec2312D66d6f84C1; // ERC20Rails Linea
-    // address public mintModule = 0x966aD227192e665960A2d1b89095C16286Fc7792; // FreeMintController Linea
-    // address public tokenFactory = 0x66B28Cc146A1a2cDF1073C2875D070733C7d01Af; // tokenFactory Linea
+    // CONFIGURE BEFORE DEPLOYING
+    address public owner = 0x016562aA41A8697720ce0943F003141f5dEAe006;
+    string public name = "DonkeyPoints";
+    string public symbol = "DP";
 
-    /// @notice GOERLI: v1.0.0
-    address public coreImpl = 0xe0dd2F320290d04Dce5432E6ec2312D66d6f84C1; // ERC20Rails goerli
-    address public mintModule = 0x8E019DfdA444743CA58065bd9b24Bd569b61fa75; // Free mint goerli
-    address public tokenFactory = 0x66B28Cc146A1a2cDF1073C2875D070733C7d01Af; // tokenFactory goerli
-
-    address public owner = ScriptUtils.symmetry;
-    string public name = "Symmetry Testing II";
-    string public symbol = "SYMII";
+    /// @notice Checkout lib/protocol-ops vX.Y.Z to automatically get addresses
+    DeploysJson deploys = ScriptUtils.getDeploysJsonStruct();
+    address erc20CoreImpl = deploys.ERC20Rails;
+    address tokenFactory = deploys.TokenFactoryProxy;
+    address erc20FreeMintController = deploys.ERC20FreeMintController;
 
     function run() public {
         vm.startBroadcast();
@@ -37,7 +31,7 @@ contract CreateERC20 is ScriptUtils {
         bytes memory permitTurnkeyMintPermit =
             abi.encodeWithSelector(Permissions.addPermission.selector, Operations.MINT_PERMIT, turnkey);
         bytes memory permitModuleMint =
-            abi.encodeWithSelector(Permissions.addPermission.selector, Operations.MINT, mintModule);
+            abi.encodeWithSelector(Permissions.addPermission.selector, Operations.MINT, erc20FreeMintController);
         bytes memory permitFrogAdmin =
             abi.encodeWithSelector(Permissions.addPermission.selector, Operations.ADMIN, frog);
         bytes memory permitSymAdmin =
@@ -52,7 +46,7 @@ contract CreateERC20 is ScriptUtils {
 
         bytes memory initData = abi.encodeWithSelector(Multicall.multicall.selector, initCalls);
 
-        TokenFactory(tokenFactory).createERC20(payable(coreImpl), owner, name, symbol, initData);
+        TokenFactory(tokenFactory).createERC20(payable(erc20CoreImpl), owner, name, symbol, initData);
 
         vm.stopBroadcast();
     }
