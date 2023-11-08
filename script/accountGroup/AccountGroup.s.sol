7aa04c11379a1de408eb2fb31aa7f3af2a48d7f1
robriks <marsterlund@gmail.com>
2023-11-07 21:46:47 -0500

    :hammer: Add ERC721AccountRails deployment to full accountgroup infra deploy script

--- script/accountGroup/AccountGroup.s.sol
+++ script/accountGroup/AccountGroup.s.sol
@@ -10,7 +10,9 @@ import {IPermissions} from "0xrails/access/permissions/Permissions.sol";
 import {AccountGroup} from "../../src/accountGroup/implementation/AccountGroup.sol";
 import {PermissionGatedInitializer} from "../../src/accountGroup/initializer/PermissionGatedInitializer.sol";
 import {InitializeAccountController} from "../../src/accountGroup/module/InitializeAccountController.sol";
+import {ERC721AccountRails} from "0xrails/cores/ERC721Account/ERC721AccountRails.sol";
 
+/// @dev Script to deploy entire AccountGroup infra to new chains
 contract AccountGroupScript is ScriptUtils {
     /*=================
         ENVIRONMENT
@@ -21,6 +23,7 @@ contract AccountGroupScript is ScriptUtils {
     AccountGroup accountGroup; // proxy
     PermissionGatedInitializer permissionGatedInitializer;
     InitializeAccountController initializeAccountController;
+    ERC721AccountRails erc721AccountRails;
 
     function run() public {
         /*===============
@@ -32,28 +35,34 @@ contract AccountGroupScript is ScriptUtils {
         address owner = ScriptUtils.stationFounderSafe;
 
         bytes32 salt = ScriptUtils.create2Salt;
-        string memory saltString = Strings.toHexString(uint256(salt), 32);
 
         // begin deployments
         (accountGroupImpl, accountGroup) = deployAccountGroup(salt, owner);
         permissionGatedInitializer = new PermissionGatedInitializer{salt: salt}();
         initializeAccountController = new InitializeAccountController{salt: salt}();
+        erc721AccountRails = new ERC721AccountRails{salt: salt}(ScriptUtils.entryPointAddress);
 
-        // After deployments, format Multicall3 calls and execute it from FounderSafe as module sender
-
-        // LOCAL DEPLOY
+        // LOCAL CONFIG
 
         // accountGroup.setDefaultAccountInitializer(address(permissionGatedInitializer));
+        // accountGroup.setDefaultAccountImplementation(address(erc721AccountRails));
         // accountGroup.addPermission(Operations.INITIALIZE_ACCOUNT, address(initializeAccountController));
         // accountGroup.addPermission(Operations.INITIALIZE_ACCOUNT_PERMIT, ScriptUtils.turnkey);
 
-        // PRODUCTION DEPLOY
+        // PRODUCTION CONFIG
 
+        // After deployments, format Multicall3 calls and execute it from FounderSafe as module sender
         bytes memory setDefaultAccountInitializer = abi.encodeWithSelector(
             AccountGroup.setDefaultAccountInitializer.selector, address(permissionGatedInitializer)
         );
         Call3 memory accountGroupSetDefaultAccountInitializerCall =
             Call3({target: address(accountGroup), allowFailure: false, callData: setDefaultAccountInitializer});
+
+        bytes memory setDefaultAccountImplementation = abi.encodeWithSelector(
+            AccountGroup.setDefaultAccountImplementation.selector, address(erc721AccountRails)
+        );
+        Call3 memory accountGroupSetDefaultAccountImplementationCall =
+            Call3({target: address(accountGroup), allowFailure: false, callData: setDefaultAccountImplementation});
 
         bytes memory addPermissionInitializeAccountToController = abi.encodeWithSelector(
             IPermissions.addPermission.selector, Operations.INITIALIZE_ACCOUNT, address(initializeAccountController)
@@ -67,10 +76,11 @@ contract AccountGroupScript is ScriptUtils {
         Call3 memory addPermissionInitializeAccountPermitToTurnkeyCall =
             Call3({target: address(accountGroup), allowFailure: false, callData: addPermissionInitializeAccountPermitToTurnkey});
 
-        Call3[] memory calls = new Call3[](3);
+        Call3[] memory calls = new Call3[](4);
         calls[0] = accountGroupSetDefaultAccountInitializerCall;
-        calls[1] = addPermissionInitializeAccountToControllerCall;
-        calls[2] = addPermissionInitializeAccountPermitToTurnkeyCall;
+        calls[1] = accountGroupSetDefaultAccountImplementationCall;
+        calls[2] = addPermissionInitializeAccountToControllerCall;
+        calls[3] = addPermissionInitializeAccountPermitToTurnkeyCall;
 
         bytes memory multicallData = abi.encodeWithSignature("aggregate3((address,bool,bytes)[])", calls);
         // `Safe(owner).execTransactionFromModule(multicall3, 0, multicallData, uint8(1));` using 0 ETH value & Operation == DELEGATECALL
@@ -82,8 +92,10 @@ contract AccountGroupScript is ScriptUtils {
 
         // assert accountGroup call successful
         assert(accountGroup.getDefaultAccountInitializer() == address(permissionGatedInitializer));
+        assert(accountGroup.getDefaultAccountImplementation() == address(erc721AccountRails));
         assert(accountGroup.hasPermission(Operations.INITIALIZE_ACCOUNT, address(initializeAccountController)));
         assert(accountGroup.hasPermission(Operations.INITIALIZE_ACCOUNT_PERMIT, ScriptUtils.turnkey));
+        assert(erc721AccountRails.initialized() == true);
 
         vm.stopBroadcast();
 
@@ -91,6 +103,7 @@ contract AccountGroupScript is ScriptUtils {
         logAddress("AccountGroupProxy @", Strings.toHexString(address(accountGroup)));
         logAddress("PermissionGatedInitializer @", Strings.toHexString(address(permissionGatedInitializer)));
         logAddress("InitializeAccountController @", Strings.toHexString(address(initializeAccountController)));
+        logAddress("ERC721AccountRails @", Strings.toHexString(address(erc721AccountRails)));
     }
 
     function deployAccountGroup(bytes32 _salt, address _owner)
