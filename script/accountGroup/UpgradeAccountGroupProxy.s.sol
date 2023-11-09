ed3e8372e79cfb7a6017cd8e95b98fdba7d01d61
Markus Osterlund / robriks <80549215+robriks@users.noreply.github.com>
2023-11-08 23:59:15 -0500

    v1.3.0- AccountGroup Deployments (#77)
    * :art: Updated 0xrails with interface, import, error changes
    
    * :white_check_mark: Enable subgroups to limit upgrades to pre-approved implementations stored in AccountGroups.sol
    
    * :white_check_mark: Add test case for AccountGroup::removeApprovedImplementation and fix bug not writing to storage
    
    * :white_check_mark: Implement basic ERC6551 'state' mechanism a la contract nonce, test case showing execution increments state
    
    * :sparkles: Add support for upgrades on non-origin chains and deprecate code for approved implementations
    
    * :white_check_mark: Add tests for new non-origin upgrades via AccountGroup admins, comment out test for deprecated functionality
    
    * :white_check_mark: Delete test of deprecated functionality
    
    * :safety_vest: Add approved implementation back into AccountGroup and fix straggler tests
    
    * :sparkles: Move upgrade validation logic to the AccountGroup- check for approved implementation and sender authority
    
    * :white_check_mark: Update tests to demonstrate upgrades can be performed either as owner or admin when default implementation has been set
    
    * :arrow_up: Use refactored _checkXXX function visibility to comply with ERC6551 state requirements
    
    * :white_check_mark: Added fork tests using goerli&mainnet to test cross-chain account upgrades
    
    * :rocket: Add 0xrails ERC721AccountRails deploy files
    
    * :hammer: Add script to upgrade existing AccountGroup proxies to a freshly deployed AccountGroupImpl
    
    * :hammer: Add ERC721AccountRails deployment to full accountgroup infra deploy script
    
    * :rocket: AccountGroup Goerli+Polygon deploy commit, add new addresses to protocol-ops json
    
    * :arrow_up: Update 0xRails dep to use 4337 eth_sign requirement
    
    * :arrow_up: Update dependencies to v1.3.0
    
    * :arrow_up: Add deploy instructions and tweak verification in protocol-ops
    
    * :memo: Finalize Sepolia deployment documentation

---
+++ script/accountGroup/UpgradeAccountGroupProxy.s.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ScriptUtils} from "protocol-ops/script/ScriptUtils.sol";
+import {JsonManager} from "protocol-ops/script/lib/JsonManager.sol";
+import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
+import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {UUPSUpgradeable} from "openzeppelin-contracts/proxy/utils/UUPSUpgradeable.sol";
+import {Operations} from "0xrails/lib/Operations.sol";
+import {IPermissions} from "0xrails/access/permissions/Permissions.sol";
+
+import {AccountGroup} from "../../src/accountGroup/implementation/AccountGroup.sol";
+import {PermissionGatedInitializer} from "../../src/accountGroup/initializer/PermissionGatedInitializer.sol";
+import {InitializeAccountController} from "../../src/accountGroup/module/InitializeAccountController.sol";
+import {ERC721AccountRails} from "0xrails/cores/ERC721Account/ERC721AccountRails.sol";
+
+/// @dev Script to deploy entire AccountGroup infra to new chains
+contract UpgradeAccountGroupProxyScript is ScriptUtils {
+    /*=================
+        ENVIRONMENT
+    =================*/
+
+    // The following contracts will be deployed:
+    AccountGroup accountGroupImpl;
+
+
+    /// @notice Checkout lib/protocol-ops vX.Y.Z to automatically get addresses
+    JsonManager.DeploysJson $deploys = setDeploysJsonStruct();
+    address owner = $deploys.StationFounderSafe;
+    address erc721AccountRails = $deploys.ERC721AccountRails;
+    
+    // The following contracts will be upgraded:
+    address accountGroup = $deploys.AccountGroupProxy; // production proxy
+
+    bytes upgradeData; // configure if a function call is desired with the upgrade
+
+
+    function run() public {
+        /*===============
+            BROADCAST
+        ===============*/
+
+        vm.startBroadcast();
+
+        bytes32 salt = ScriptUtils.create2Salt;
+
+        // begin deployments
+        accountGroupImpl = new AccountGroup{salt: salt}();
+
+        bytes memory upgradeCall = abi.encodeWithSelector(
+            UUPSUpgradeable.upgradeToAndCall.selector, address(accountGroupImpl), upgradeData
+        );
+
+        Call3 memory accountGroupUpgradeCall =
+            Call3({target: accountGroup, allowFailure: false, callData: upgradeCall});
+        
+        Call3[] memory calls = new Call3[](1);
+        calls[0] = accountGroupUpgradeCall;
+
+        bytes memory multicallData = abi.encodeWithSignature("aggregate3((address,bool,bytes)[])", calls);
+        // `Safe(owner).execTransactionFromModule(multicall3, 0, multicallData, uint8(1));` using 0 ETH value & Operation == DELEGATECALL
+        bytes memory safeCall = abi.encodeWithSignature(
+            "execTransactionFromModule(address,uint256,bytes,uint8)", multicall3, 0, multicallData, uint8(1)
+        );
+        (bool r,) = owner.call(safeCall);
+        require(r);
+
+        assert(AccountGroup(accountGroup).getDefaultAccountImplementation() == erc721AccountRails);
+
+        vm.stopBroadcast();
+
+        logAddress("NewAccountGroupImpl @", Strings.toHexString(address(accountGroupImpl)));
+    }
+}
